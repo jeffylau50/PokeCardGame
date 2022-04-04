1691d400b19428bafd308ce9c99a3b82f5213986
Jeffy Lau <100321067+jeffy2560@users.noreply.github.com>
2022-04-04 15:36:42 -0700

    component linked

---
+++ src/deck.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Card from './card.js'
+
+class Deck extends Component {
+    
+
+    render(){
+        return(
+            <Card />
+        )
+    }
+}
+
+export default Deck;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Deck from './deck.js'
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+        <Deck />
+        )
+    }
+
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
