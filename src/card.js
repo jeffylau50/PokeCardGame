39dcc4e3817a2ad14f661c199b72d6d180e70d1a
Jeffy Lau <100321067+jeffy2560@users.noreply.github.com>
2022-04-05 14:24:00 -0700

    css update

--- src/card.js
+++ src/card.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import './card.css'
 
 class Card extends Component {
+    constructor(props) {
+        super(props);
+        let angle = Math.random()* 90-45;
+        let xpos = Math.random()* 40-20;
+        let y = Math.random()* 40-20;
+        this._transform = `translate(${xpos}px, ${y}px) rotate(${angle}deg)`;
+    };
 
-    render(){
+   
+    render(){ 
+        
+        
         return(
             <div >
-            <img className='card' src={this.props.img}/>
+            <img className='card' style={{transform: this._transform}} src={this.props.img}/>
             </div>
         )
     }
